2ff6c380b83eb665f148462cabf1d589e5a6054e
Ruben Svensson <ruben.ne.svensson@gmail.com>
2022-11-29 21:14:26 +0100

    Removed repos

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,24 +31,6 @@ export default function Home() {
           </Link>
         </div>
       </div>
-
-      <div>
-        <div className={styles.container}>
-          <h4>God Jul as we say in Swedish.</h4>
-        </div>
-        <div className={styles.container}>
-          <h4>Cool repositories</h4>
-          <Link href="https://github.com/goonstation/goonstation">
-            <h3>Goonstation</h3>
-          </Link>
-          <Link href="https://github.com/awesome-selfhosted/awesome-selfhosted">
-            <h3>Awesome-Selfhosted</h3>
-          </Link>
-          <Link href="https://github.com/AI-Tournaments/AI-Tournaments.github.io">
-            <h3>AI-Tournaments</h3>
-          </Link>
-        </div>
-      </div>
     </div>
   );
 }
